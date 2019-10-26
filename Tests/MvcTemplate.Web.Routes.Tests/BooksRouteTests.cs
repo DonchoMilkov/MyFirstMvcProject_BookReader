1564757b30eb5689319878c05c7122a7b719a95e
Doncho Milkov <donchomilkov@gmail.com>
2019-10-26 15:25:42 +0300

    Route test added

---
+++ Tests/MvcTemplate.Web.Routes.Tests/BooksRouteTests.cs
@@ -0,0 +1,20 @@
+﻿namespace MvcTemplate.Web.Routes.Tests
+{
+    using System.Web.Routing;
+    using MvcRouteTester;
+    using MvcTemplate.Web.Controllers;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BooksRouteTests
+    {
+        [Test]
+        public void TestRouteByID()
+        {
+            var routeCollection = new RouteCollection();
+            const string url = "/Book/MTQuMzQyMzM0MjM0";
+            RouteConfig.RegisterRoutes(routeCollection);
+            routeCollection.ShouldMap(url).To<BooksController>(c => c.ById("MTQuMzQyMzM0MjM0"));
+        }
+    }
+}
